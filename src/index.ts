94522130fa87b6e36c998d93fc64254863d6d125
jimmyxuexue <1002661758@qq.com>
2022-04-23 23:32:10 +0800

    fix :bug: hooks文件改回ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { default as useSessionStroageState } from './useSessionStroageState';
 export { default as useLocalStorageState } from './useLocalStorageState';
 export { default as useLoading } from './useLoading';
+export { default as useForceUpdate } from './useForceUpdate';
+export { default as useFetch } from './useFetch';
 
 import 'antd/dist/antd.css';
 
